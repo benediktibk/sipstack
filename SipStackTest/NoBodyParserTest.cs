d9db321cf7e7c2f7b1809fa823cb417d0d8b8831
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-11 20:44:52 +0200

    added test for NoBodyParser

---
+++ SipStackTest/NoBodyParserTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SipStack;
+using FluentAssertions;
+
+namespace SipStackTest
+{
+    [TestClass]
+    public class NoBodyParserTest
+    {
+        private NoBodyParser _noBodyParser;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _noBodyParser = new NoBodyParser();
+        }
+
+        [TestMethod]
+        public void Parse_NullValues_NoBody()
+        {
+            var result = _noBodyParser.Parse(null, 0, 0);
+
+            result.Result.Should().BeOfType(typeof(NoBody));
+        }
+    }
+}
