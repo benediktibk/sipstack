85c614e2d03dc565b9a8f3388d388843948fa523
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-14 23:46:23 +0200

    simplified parsing through usage of regular expressions

--- SipStack/Header/HeaderFieldParser.cs
+++ SipStack/Header/HeaderFieldParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SipStack.Header
 {
@@ -14,71 +15,29 @@ namespace SipStack.Header
             if (string.IsNullOrEmpty(startLine))
                 return new ParseResult<HeaderField>($"empty header line");
 
-            var indexOfDelimiter = startLine.IndexOf(':');
-            var indexOfWhitespace = startLine.IndexOf(' ');
+            var pattern = @"([^  \t]*)[ \t]*:[ \t]*(.*)";
+            var matches = Regex.Matches(lines[start], pattern);
 
-            if (indexOfDelimiter < 0)
-                return new ParseResult<HeaderField>($"missing colon in header field: {startLine}");
+            if (matches.Count != 1)
+                return new ParseResult<HeaderField>($"the header line '{lines[start]}' is malformed");
 
-            var nameStart = 0;
-            var nameEnd = 0;
-            var valueStart = 0;
-            var valueEnd = 0;
+            var fieldName = new HeaderFieldName(matches[0].Groups[1].Value);
+            var stringBuilder = new StringBuilder(matches[0].Groups[2].Value);
+            pattern = @"^(\.|[ \t]+)([^ \t].*)$|^[ \t]*$";
 
-            // no whitespaces at all (Subject:lunch)
-            if (indexOfWhitespace < 0)
+            while (end + 1 < lines.Count)
             {
-                nameStart = 0;
-                nameEnd = indexOfDelimiter - 1;
-                valueStart = indexOfDelimiter + 1;
-                valueEnd = startLine.Length - 1;
-            }
-            // whitespace after colon (Subject: lunch)
-            else if (indexOfWhitespace > indexOfDelimiter)
-            {
-                nameStart = 0;
-                nameEnd = indexOfDelimiter - 1;
-                var indexOfNoneWhitespace = IndexOfNoneWhitespace(startLine, indexOfWhitespace);
+                matches = Regex.Matches(lines[end + 1], pattern);
 
-                if (indexOfNoneWhitespace < 0)
-                    indexOfNoneWhitespace = startLine.Length;
+                if (matches.Count == 0)
+                    break;
 
-                valueStart = indexOfNoneWhitespace;
-                valueEnd = startLine.Length - 1;
-            }
-            // whitespace before colon (Subject    :  lunch)
-            else
-            {
-                nameStart = 0;
-                nameEnd = indexOfWhitespace - 1;
-                var indexOfNoneWhitespace = IndexOfNoneWhitespace(startLine, indexOfDelimiter + 1);
+                end = end + 1;
 
-                if (indexOfNoneWhitespace < 0)
-                    indexOfNoneWhitespace = startLine.Length;
+                if (matches[0].Groups.Count > 1)
+                    stringBuilder.AppendFormat(" {0}", matches[0].Groups[2].Value);
+            }       
 
-                valueStart = indexOfNoneWhitespace;
-                valueEnd = startLine.Length - 1;
-            }
-
-            var additionalLines = CountNextLinesWithWhitespaceOrDotInFront(lines, start + 1);
-            end = start + additionalLines;
-            var stringBuilder = new StringBuilder(startLine.Substring(valueStart, valueEnd - valueStart + 1));
-
-            for (var i = 0; i < additionalLines; ++i)
-            {
-                var currentLine = lines[start + 1 + i];
-                var lineStart = IndexOfNoneWhitespace(currentLine, 1);
-
-                if (lineStart < 0)
-                {
-                    stringBuilder.Append(" ");
-                    continue;
-                }
-                
-                stringBuilder.AppendFormat(" {0}", currentLine.Substring(lineStart));
-            }            
-
-            var fieldName = new HeaderFieldName(startLine.Substring(nameStart, nameEnd - nameStart + 1));
             var fieldValues = stringBuilder.ToString();
             var fieldValuesAsList = SeparateFieldValues(fieldName, fieldValues);
 
@@ -118,27 +77,6 @@ namespace SipStack.Header
             }
             
             return values;
-        }
-
-        private static int CountNextLinesWithWhitespaceOrDotInFront(IList<string> lines, int start)
-        {
-            var result = 0; 
-
-            for(var i = start; i < lines.Count; ++i)
-            {
-                var currentLine = lines[i];
-                result = i - start;
-
-                if (currentLine.Length == 0)
-                    return result;
-
-                var firstCharacter = currentLine[0];
-
-                if (firstCharacter != ' ' && firstCharacter != '\t' && firstCharacter != '.')
-                    return result;
-            }
-
-            return lines.Count - start;
         }
 
         private static int IndexOfNoneWhitespace(string line, int start)
