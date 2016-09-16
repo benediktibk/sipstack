3d94673714a84473164f9c3613104b222d9fb505
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-16 18:22:27 +0200

    implemented attribute line

--- SipStack/Body/Sdp/AttributeLIne.cs
+++ SipStack/Body/Sdp/AttributeLIne.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace SipStack.Body.Sdp
 {
     public class AttributeLine : ILine
     {
+        private readonly string _name;
+        private readonly string _value;
+
+        public AttributeLine(string name)
+        {
+            _name = name;
+            _value = "";
+        }
+
+        public AttributeLine(string name, string value)
+        {
+            _name = name;
+            _value = value;
+        }
+        
+        public string Name => _name;
+        public string Value => _value;
+
         public static ParseResult<ILine> CreateFrom(string data)
         {
-            throw new NotImplementedException();
+            var pattern = @"^([^:]*):?(.*)$";
+            var matches = Regex.Matches(data, pattern);
+
+            if (matches.Count != 1)
+                return new ParseResult<ILine>($"attribute line '{data}' is invalid");
+
+            var name = matches[0].Groups[1].Value;
+            var value = matches[0].Groups[2].Value;
+
+            return new ParseResult<ILine>(new AttributeLine(name, value));
         }
     }
 }
---
+++ SipStackTest/Body/Sdp/AttributeLineTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SipStack.Body.Sdp;
+using FluentAssertions;
+using SipStack.Network;
+using System.Net;
+
+namespace SipStackTest.Body.Sdp
+{
+    [TestClass]
+    public class AttributeLineTest
+    {
+        [TestMethod]
+        public void CreateFrom_recvonly_NameAndValueAreCorrect()
+        {
+            var line = AttributeLine.CreateFrom(@"recvonly");
+
+            var attributeLine = line.Result as AttributeLine;
+            attributeLine.Name.Should().Be("recvonly");
+            attributeLine.Value.Should().Be("");
+        }
+
+        [TestMethod]
+        public void CreateFrom_rtpmap_NameAndValueAreCorrect()
+        {
+            var line = AttributeLine.CreateFrom(@"rtpmap:8 PCMA/8000/1");
+
+            var attributeLine = line.Result as AttributeLine;
+            attributeLine.Name.Should().Be("rtpmap");
+            attributeLine.Value.Should().Be("8 PCMA/8000/1");
+        }
+    }
+}
