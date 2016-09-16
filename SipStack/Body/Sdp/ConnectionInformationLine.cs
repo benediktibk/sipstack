ab2cb5a5f48ad4180ed3f4171f5683569761de86
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-16 18:52:27 +0200

    added tests for connection information line

--- SipStack/Body/Sdp/ConnectionInformationLine.cs
+++ SipStack/Body/Sdp/ConnectionInformationLine.cs
@@ -1,16 +1,43 @@
-﻿using System;
+﻿using SipStack.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SipStack.Body.Sdp
 {
     public class ConnectionInformationLine : ILine
     {
+        private readonly NetType _netType;
+        private readonly AddressType _addressType;
+        private readonly IPAddress _ipAddress;
+        private readonly int _multicastTimeToLive;
+        private readonly int _numberOfMulticastAddresses;
+
+        public ConnectionInformationLine(NetType netType, AddressType addressType, IPAddress ipAddress, int multicastTimeToLive, int numberOfMulticastAddresses)
+        {
+            _netType = netType;
+            _addressType = addressType;
+            _ipAddress = ipAddress;
+            _multicastTimeToLive = multicastTimeToLive;
+            _numberOfMulticastAddresses = numberOfMulticastAddresses;
+        }
+
+        public NetType NetType => _netType;
+        public AddressType AddressType => _addressType;
+        public IPAddress IPAddress => _ipAddress;
+        public int MulticastTimeToLive => _multicastTimeToLive;
+        public int NumberOfMulticastAddresses => _numberOfMulticastAddresses;
+
         public static ParseResult<ILine> CreateFrom(string data)
         {
-            throw new NotImplementedException();
+            var pattern = @"^(.*) (.*) ([a-zA-Z\.:0-9]*)\/?([0-9]*)\/?([0-9]*)$";
+            var matches = Regex.Matches(data, pattern);
+
+            return new ParseResult<ILine>($"the connection information '{data}' is invalid");
         }
     }
 }
---
+++ SipStackTest/Body/Sdp/ConnectionInformationLineTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using SipStack.Body.Sdp;
+using SipStack.Network;
+using System.Net;
+
+namespace SipStackTest.Body.Sdp
+{
+    [TestClass]
+    public class ConnectionInformationLineTest
+    {
+        [TestMethod]
+        public void CreateFrom_Ipv4Unicast_AllValuesAreCorrect()
+        {
+            var line = ConnectionInformationLine.CreateFrom("IN IP4 10.122.69.145");
+
+            var connectionInformationLine = line.Result as ConnectionInformationLine;
+            connectionInformationLine.NetType.Should().Be(NetType.Internet);
+            connectionInformationLine.AddressType.Should().Be(AddressType.Ipv4);
+            connectionInformationLine.IPAddress.Should().Be(IPAddress.Parse("10.122.69.145"));
+            connectionInformationLine.MulticastTimeToLive.Should().Be(0);
+            connectionInformationLine.NumberOfMulticastAddresses.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void CreateFrom_Ipv6Unicast_AllValuesAreCorrect()
+        {
+            var line = ConnectionInformationLine.CreateFrom("IN IP6 0015::101");
+
+            var connectionInformationLine = line.Result as ConnectionInformationLine;
+            connectionInformationLine.NetType.Should().Be(NetType.Internet);
+            connectionInformationLine.AddressType.Should().Be(AddressType.Ipv6);
+            connectionInformationLine.IPAddress.Should().Be(IPAddress.Parse("0015::101"));
+            connectionInformationLine.MulticastTimeToLive.Should().Be(0);
+            connectionInformationLine.NumberOfMulticastAddresses.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void CreateFrom_Ipv6Multicast_AllValuesAreCorrect()
+        {
+            var line = ConnectionInformationLine.CreateFrom("IN IP6 FF15::101");
+
+            var connectionInformationLine = line.Result as ConnectionInformationLine;
+            connectionInformationLine.NetType.Should().Be(NetType.Internet);
+            connectionInformationLine.AddressType.Should().Be(AddressType.Ipv6);
+            connectionInformationLine.IPAddress.Should().Be(IPAddress.Parse("FF15::101"));
+            connectionInformationLine.MulticastTimeToLive.Should().Be(0);
+            connectionInformationLine.NumberOfMulticastAddresses.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void CreateFrom_Ipv6MulticastMultipleAddresses_AllValuesAreCorrect()
+        {
+            var line = ConnectionInformationLine.CreateFrom("IN IP6 FF15::101/3");
+
+            var connectionInformationLine = line.Result as ConnectionInformationLine;
+            connectionInformationLine.NetType.Should().Be(NetType.Internet);
+            connectionInformationLine.AddressType.Should().Be(AddressType.Ipv6);
+            connectionInformationLine.IPAddress.Should().Be(IPAddress.Parse("FF15::101"));
+            connectionInformationLine.MulticastTimeToLive.Should().Be(0);
+            connectionInformationLine.NumberOfMulticastAddresses.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void CreateFrom_Ipv4MulticastWithTtl_AllValuesAreCorrect()
+        {
+            var line = ConnectionInformationLine.CreateFrom("IN IP4 224.2.36.42/127");
+
+            var connectionInformationLine = line.Result as ConnectionInformationLine;
+            connectionInformationLine.NetType.Should().Be(NetType.Internet);
+            connectionInformationLine.AddressType.Should().Be(AddressType.Ipv4);
+            connectionInformationLine.IPAddress.Should().Be(IPAddress.Parse("224.2.36.42"));
+            connectionInformationLine.MulticastTimeToLive.Should().Be(127);
+            connectionInformationLine.NumberOfMulticastAddresses.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void CreateFrom_Ipv4MulticastWithTtlAndMultipleAddress_AllValuesAreCorrect()
+        {
+            var line = ConnectionInformationLine.CreateFrom("IN IP4 224.2.36.42/127/2");
+
+            var connectionInformationLine = line.Result as ConnectionInformationLine;
+            connectionInformationLine.NetType.Should().Be(NetType.Internet);
+            connectionInformationLine.AddressType.Should().Be(AddressType.Ipv4);
+            connectionInformationLine.IPAddress.Should().Be(IPAddress.Parse("224.2.36.42"));
+            connectionInformationLine.MulticastTimeToLive.Should().Be(127);
+            connectionInformationLine.NumberOfMulticastAddresses.Should().Be(2);
+        }
+    }
+}
