7c1256cd4d9f272fbf1251229fed36b39e73b3b4
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-18 12:41:05 +0200

    implemented parser for time line

--- SipStack/Body/Sdp/TimeLine.cs
+++ SipStack/Body/Sdp/TimeLine.cs
@@ -1,16 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SipStack.Body.Sdp
 {
     public class TimeLine : ILine
     {
+        private readonly DateTime _start;
+        private readonly DateTime _end;
+
+        public TimeLine(DateTime start, DateTime end)
+        {
+            if (DateTime.Compare(start, end) >= 0)
+                throw new ArgumentException("end must be after start");
+
+            _start = start;
+            _end = end;
+        }
+
+        public DateTime Start => _start;
+        public DateTime End => _end;
+
         public static ParseResult<ILine> CreateFrom(string data)
         {
-            throw new NotImplementedException();
+            var pattern = @"^([0-9]*) ([0-9]*)$";
+            var matches = Regex.Matches(data, pattern);
+
+            if (matches.Count != 1)
+                return new ParseResult<ILine>($"the data '{data}' for the time schedule is inalid");
+
+            var match = matches[0];
+            var startString = match.Groups[1].Value;
+            var endString = match.Groups[2].Value;
+            long startInteger;
+            long endInteger;
+
+            if (!long.TryParse(startString, out startInteger))
+                return new ParseResult<ILine>($"the value '{startString}' for start is invalid");
+
+            if (!long.TryParse(endString, out endInteger))
+                return new ParseResult<ILine>($"the value '{endInteger}' for start is invalid");
+
+            if (startInteger <= 0)
+                return new ParseResult<ILine>("the value for start must not be negative");
+
+            if (endInteger < 0)
+                return new ParseResult<ILine>("the value for start must be positive");
+
+            if (endInteger > 0 && startInteger > endInteger)
+                return new ParseResult<ILine>("the value for start must be less or equal than end");
+
+            var start = new DateTime(startInteger);
+            var end = DateTime.MaxValue;
+
+            if (endInteger > 0)
+                end = new DateTime(endInteger);
+
+            return new ParseResult<ILine>(new TimeLine(start, end));
         }
     }
 }
---
+++ SipStackTest/Body/Sdp/TimeLineTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using SipStack.Body.Sdp;
+using System;
+
+namespace SipStackTest.Body.Sdp
+{
+    [TestClass]
+    public class TimeLineTest
+    {
+        [TestMethod]
+        public void CreateFrom_PositiveStartAndEnd_AllValuesAreCorrect()
+        {
+            var line = TimeLine.CreateFrom(@"2873397496 2873404696");
+
+            var timeLine = line.Result as TimeLine;
+            timeLine.Start.Ticks.Should().Be(2873397496L);
+            timeLine.End.Ticks.Should().Be(2873404696L);
+        }
+
+        [TestMethod]
+        public void CreateFrom_PositiveStartAndZeroEnd_AllValuesAreCorrect()
+        {
+            var line = TimeLine.CreateFrom(@"2873397496 0");
+
+            var timeLine = line.Result as TimeLine;
+            timeLine.Start.Ticks.Should().Be(2873397496L);
+            timeLine.End.Ticks.Should().Be(DateTime.MaxValue.Ticks);
+        }
+
+        [TestMethod]
+        public void CreateFrom_NegativeStartAndPositiveEnd_Error()
+        {
+            var line = TimeLine.CreateFrom(@"-2873397496 2873404696");
+
+            line.IsError.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void CreateFrom_PositiveStartAndNegativeEnd_Error()
+        {
+            var line = TimeLine.CreateFrom(@"2873397496 -2873404696");
+
+            line.IsError.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void CreateFrom_EndBeforeStart_Error()
+        {
+            var line = TimeLine.CreateFrom(@"2973397496 2873404696");
+
+            line.IsError.Should().BeTrue();
+        }
+    }
+}
