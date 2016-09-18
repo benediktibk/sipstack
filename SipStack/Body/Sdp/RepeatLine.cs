c9d26b3f3eb5f7b81dbf549adf6a8258ede5fd46
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-18 17:34:21 +0200

    implemented parsing of repeat line

--- SipStack/Body/Sdp/RepeatLine.cs
+++ SipStack/Body/Sdp/RepeatLine.cs
@@ -42,7 +42,34 @@ namespace SipStack.Body.Sdp
 
         public static ParseResult<ILine> CreateFrom(string data)
         {
-            throw new NotImplementedException();
+            var pattern = @"^([0-9dhms]*) ([0-9dhms]*) ([0-9dhms]*) ([0-9dhms]*)$";
+            var matches = Regex.Matches(data, pattern);
+
+            if (matches.Count != 1)
+                return new ParseResult<ILine>($"the data '{data}' for the repeat line is malformed");
+
+            var match = matches[0];
+            var repeatInterval = CreateTimeSpanFrom(match.Groups[1].Value);
+            var activeDuration = CreateTimeSpanFrom(match.Groups[2].Value);
+            var offsetStart = CreateTimeSpanFrom(match.Groups[3].Value);
+            var offsetEnd = CreateTimeSpanFrom(match.Groups[4].Value);
+
+            if (repeatInterval.IsError)
+                return repeatInterval.ToParseResult<ILine>();
+
+            if (activeDuration.IsError)
+                return activeDuration.ToParseResult<ILine>();
+
+            if (offsetStart.IsError)
+                return offsetStart.ToParseResult<ILine>();
+
+            if (offsetEnd.IsError)
+                return offsetEnd.ToParseResult<ILine>();
+
+            if (TimeSpan.Compare(offsetStart.Result, offsetEnd.Result) > 0)
+                return new ParseResult<ILine>("the value for offset start must be less or equal than the value for offset end");
+
+            return new ParseResult<ILine>(new RepeatLine(repeatInterval.Result, activeDuration.Result, offsetStart.Result, offsetEnd.Result));
         }
 
         public static ParseResult<TimeSpan> CreateTimeSpanFrom(string data)
