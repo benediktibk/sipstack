1de7797832b8cd5b6f0843c60cc1f18ea1721069
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-09-18 21:43:18 +0200

    added regular expressions for phone numbers

--- SipStack/Utils/PhoneNumber.cs
+++ SipStack/Utils/PhoneNumber.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace SipStack.Utils
 {
     public class PhoneNumber
     {
+        #region privateStaticVariables
+
+        private static readonly string _charactersAllowedForPhoneNumber = @"[0-9\+ \-\/\\]";
+        private static readonly string _charactersAllowedForDomain = @"[^ @]";
+        private static readonly string _charactersAllowedForAlphaNumericUser = @"[^ ()<>]";
+        private static readonly string _patternPhoneNumberOnly;
+        private static readonly string _patternPhoneNumberWithDomain;
+        private static readonly string _patternPhoneNumberWithDomainAndDisplayNameAfter;
+        private static readonly string _patternPhoneNumberWithDomainAndDisplayNameBefore;
+        private static readonly string _patternAlphaNumericOnly;
+        private static readonly string _patternAlphaNumericWithDisplayNameAfter;
+        private static readonly string _patternAlphaNumericWithDisplayNameBefore;
+
+        #endregion
+
         #region private variables
 
         private readonly string _displayName;
@@ -14,6 +30,17 @@ namespace SipStack.Utils
         #endregion
 
         #region constructors
+
+        static PhoneNumber()
+        {
+            _patternPhoneNumberOnly = $@"^({_charactersAllowedForPhoneNumber}*)$";
+            _patternPhoneNumberWithDomain = $@"^({_charactersAllowedForPhoneNumber}*)@({_charactersAllowedForDomain}*)$";
+            _patternPhoneNumberWithDomainAndDisplayNameAfter = $@"^({_charactersAllowedForPhoneNumber}*)@({_charactersAllowedForDomain}*) \((.*)\)$";
+            _patternPhoneNumberWithDomainAndDisplayNameBefore = $@"^(.*) <({_charactersAllowedForPhoneNumber}*)@({_charactersAllowedForDomain}*)>$";
+            _patternAlphaNumericOnly = $@"^({_charactersAllowedForAlphaNumericUser}*)@({_charactersAllowedForDomain}*)$";
+            _patternAlphaNumericWithDisplayNameAfter = $@"^({_charactersAllowedForAlphaNumericUser}*)@({_charactersAllowedForDomain}*) \((.*)\)$";
+            _patternAlphaNumericWithDisplayNameBefore = $@"^(.*) <({_charactersAllowedForAlphaNumericUser}*)@({_charactersAllowedForDomain}*)>$";
+        }
 
         #endregion
 
@@ -30,6 +57,20 @@ namespace SipStack.Utils
         #region public static functions
 
         public static ParseResult<PhoneNumber> Parse(string data)
+        {
+            var matchPhoneNumberOnly = Regex.Match(data, _patternPhoneNumberOnly);
+
+            if (matchPhoneNumberOnly.Success)
+            {
+                var user = ParseNumericUser()
+            }
+        }
+
+        #endregion
+
+        #region private static functions
+
+        private static string ParseNumericUser(string data)
         {
             throw new NotImplementedException();
         }
