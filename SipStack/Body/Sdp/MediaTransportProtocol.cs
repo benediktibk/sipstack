cba973f66484e96fdf1ca421d825b387c4b7baa7
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-10-06 17:20:28 +0200

    Add MediaType and MediaTransportProtocol

---
+++ SipStack/Body/Sdp/MediaTransportProtocol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SipStack.Body.Sdp
+{
+    public enum MediaTransportProtocol
+    {
+        Udp,
+        RtpAvp,
+        RtpSavp
+    }
+
+    public static class MediaTransportProtocolUtils
+    {
+        private static IDictionary<string, MediaTransportProtocol> StringToType;
+        private static IDictionary<MediaTransportProtocol, string> TypeToString;
+
+        static MediaTransportProtocolUtils()
+        {
+            TypeToString = new Dictionary<MediaTransportProtocol, string>
+            {
+                { MediaTransportProtocol.Udp, "udp" },
+                { MediaTransportProtocol.RtpAvp, "RTP/AVP" },
+                { MediaTransportProtocol.RtpSavp, "RTP/SAVP" }
+            };
+
+            StringToType = TypeToString.ToDictionary(x => x.Value.ToLower(), x => x.Key);
+        }
+
+        public static bool TryParse(string value, out MediaTransportProtocol parsedValue)
+        {
+            return StringToType.TryGetValue(value.ToLower(), out parsedValue);
+        }
+
+        public static string ToFriendlyString(this MediaTransportProtocol value)
+        {
+            return TypeToString[value];
+        }
+    }
+}
---
+++ SipStack/Body/Sdp/MediaType.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SipStack.Body.Sdp
+{
+    public enum MediaType
+    {
+        Audio,
+        Video,
+        Text,
+        Application,
+        Message
+    }
+
+    public static class MediaTypeUtils
+    {
+        private static IDictionary<string, MediaType> StringToType;
+        private static IDictionary<MediaType, string> TypeToString;
+
+        static MediaTypeUtils()
+        {
+            TypeToString = new Dictionary<MediaType, string>
+            {
+                { MediaType.Audio, "audio" },
+                { MediaType.Video, "video" },
+                { MediaType.Text, "text" },
+                { MediaType.Application, "application" },
+                { MediaType.Message, "message" }
+            };
+
+            StringToType = TypeToString.ToDictionary(x => x.Value.ToLower(), x => x.Key);
+        }
+
+        public static bool TryParse(string value, out MediaType parsedValue)
+        {
+            return StringToType.TryGetValue(value.ToLower(), out parsedValue);
+        }
+
+        public static string ToFriendlyString(this MediaType value)
+        {
+            return TypeToString[value];
+        }
+    }
+}
