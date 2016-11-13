b22c92a2d7809ccad3b685315de90d923eb7617c
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-11-13 16:33:22 +0100

    Add Class TimeDescription

---
+++ SipStack/Body/Sdp/TimeDescription.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SipStack.Body.Sdp
+{
+    public class TimeDescription
+    {
+        private readonly List<RepeatLine> _repeatings;
+
+        public TimeDescription(TimeLine time, IReadOnlyList<RepeatLine> repeatings)
+        {
+            Time = time;
+            _repeatings = repeatings.ToList();
+        }
+
+        public TimeLine Time { get; }
+        public IReadOnlyList<RepeatLine> Repeatings => _repeatings;
+    }
+}
