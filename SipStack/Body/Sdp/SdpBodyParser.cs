f4e31f89cd9060de3677e4fb6c43e9fd1b58d639
Benedikt Schmidt <benedikt.schmidt@tum.de>
2016-11-13 15:19:58 +0100

    Apply different Validations according to the SDP Line Type

--- SipStack/Body/Sdp/SdpBodyParser.cs
+++ SipStack/Body/Sdp/SdpBodyParser.cs
@@ -23,16 +23,15 @@ namespace SipStack.Body.Sdp
 
             var parsedLines = parsedLinesResult.Result;
             var linesWithUsage = parsedLines.Select(x => new LineWithUsage(x)).ToList();
-            var originatorResult = ParseSessionGeneralLine<OriginatorLine>(linesWithUsage);
-            var uriResult = ParseSessionGeneralLine<UriLine>(linesWithUsage);
-            var sessionNameResult = ParseSessionGeneralLine<SessionNameLine>(linesWithUsage);
-            var sessionDescriptionResult = ParseSessionGeneralLine<DescriptionLine>(linesWithUsage);
+            var originatorResult = ParseOriginatorLine(linesWithUsage);
+            var uriResult = ParseUriLine(linesWithUsage);
+            var sessionNameResult = ParseSessionNameLine(linesWithUsage);
+            var sessionDescriptionResult = ParseSessionDescriptionLine(linesWithUsage);
 
             if (originatorResult.IsError)
                 return originatorResult.ToParseResult<IBody>();
 
-            var body = new SdpBody(originatorResult.Result, sessionNameResult.Result, sessionDescriptionResult.Result, uriResult.Result);
-            throw new NotImplementedException();        
+            var body = new SdpBody(originatorResult.Result, sessionNameResult.Result, sessionDescriptionResult.Result, uriResult.Result);       
             return new ParseResult<IBody>(body);
         }
 
@@ -53,6 +52,63 @@ namespace SipStack.Body.Sdp
             }
 
             return new ParseResult<List<ILine>>(parsedLines);
+        }
+
+        private ParseResult<OriginatorLine> ParseOriginatorLine(IReadOnlyList<LineWithUsage> lines)
+        {
+            var linesWithType = lines.Where(x => x.Line is OriginatorLine).ToList();
+
+            if (linesWithType.Count() > 1)
+                return new ParseResult<OriginatorLine>($"there must be at most one originator line");
+
+            if (linesWithType.Count() == 0)
+                return new ParseResult<OriginatorLine>(null as OriginatorLine);
+
+            var lineWithUsage = linesWithType[0];
+            lineWithUsage.MarkAsUsed();
+            return new ParseResult<OriginatorLine>(lineWithUsage.Line as OriginatorLine);
+        }
+
+        private ParseResult<UriLine> ParseUriLine(IReadOnlyList<LineWithUsage> lines)
+        {
+            var linesWithType = lines.Where(x => x.Line is UriLine).ToList();
+
+            if (linesWithType.Count() > 1)
+                return new ParseResult<UriLine>($"there must be at most one uri line");
+
+            if (linesWithType.Count() == 0)
+                return new ParseResult<UriLine>(null as UriLine);
+
+            var lineWithUsage = linesWithType[0];
+            lineWithUsage.MarkAsUsed();
+            return new ParseResult<UriLine>(lineWithUsage.Line as UriLine);
+        }
+
+        private ParseResult<SessionNameLine> ParseSessionNameLine(IReadOnlyList<LineWithUsage> lines)
+        {
+            var linesWithType = lines.Where(x => x.Line is SessionNameLine).ToList();
+
+            if (linesWithType.Count() != 1)
+                return new ParseResult<SessionNameLine>($"there must be at exactly one session name line");
+
+            var lineWithUsage = linesWithType[0];
+            lineWithUsage.MarkAsUsed();
+            return new ParseResult<SessionNameLine>(lineWithUsage.Line as SessionNameLine);
+        }
+
+        private ParseResult<DescriptionLine> ParseSessionDescriptionLine(IReadOnlyList<LineWithUsage> lines)
+        {
+            var linesWithType = lines.Where(x => x.Line is DescriptionLine).ToList();
+
+            if (linesWithType.Count() != 1)
+                return new ParseResult<DescriptionLine>($"there must be at most one general session name line");
+
+            if (linesWithType.Count() == 0)
+                return new ParseResult<DescriptionLine>(null as DescriptionLine);
+
+            var lineWithUsage = linesWithType[0];
+            lineWithUsage.MarkAsUsed();
+            return new ParseResult<DescriptionLine>(lineWithUsage.Line as DescriptionLine);
         }
 
         private ParseResult<LineType> ParseSessionGeneralLine<LineType>(IReadOnlyList<LineWithUsage> lines) 
